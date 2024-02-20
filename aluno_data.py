6120a67bb8961d0094ba5d4fd14520436b54f336
0Raposo <146982528+0Raposo@users.noreply.github.com>
2024-02-20 11:48:46 -0300

    Add files via upload

---
+++ aluno_data.py
@@ -0,0 +1,62 @@
+import pymysql.cursors
+from aluno_model import Aluno
+
+class AlunoData:
+    def __init__(self):
+        self.conexao = pymysql.connect(host= 'localhost',
+                                       user='root',
+                                       password= '',
+                                       database='escola',
+                                       cursorclass= pymysql.cursors.DictCursor)
+        self.cursor = self.conexao.cursor()
+
+    def insert(self, aluno: Aluno):
+        try:
+            sql= 'INSERT INTO alunos (matricula, nome, idade, curso, nota)'\
+                 'VALUES (%s, %s, %s, %s, %s)'
+            self.cursor.execute(sql, (aluno.matricula,
+                                     aluno.nome,
+                                     aluno.idade,
+                                     aluno.curso,
+                                     aluno.nota))
+            self.conexao.commit()
+
+        except Exception as error:
+            print(f'Erro ao cadastrar! Erro: {error} ')
+
+    def update(self, aluno: Aluno):
+        try:
+            sql = 'UPDATE alunos SET nome = %s, idade = %s, curso = %s, nota = %s '\
+                  'WHERE matricula = %s'
+            self.cursor.execute(sql, (aluno.nome,
+                                           aluno.idade,
+                                           aluno.curso,
+                                           aluno.nota,
+                                           aluno.matricula))
+            self.conexao.commit()
+        except Exception as error:
+            print(f'Erro ao cadastrar! Erro: {error} ')
+
+    def delete(self, matricula: str):
+        try:
+            sql = 'DELETE FROM alunos WHERE matricula  = %s'
+            self.cursor.execute(sql, matricula)
+
+            self.conexao.commit()
+        except Exception as error:
+            print(f'Erro ao deletar! Erro: {error}')
+
+    def select(self):
+        try:
+            sql = 'SELECT * FROM alunos '
+            self.cursor.execute(sql)
+            alunos = self.cursor.fetchall()
+            return alunos
+
+        except Exception as error:
+            print(f'Erro ao listar! erro: {error}')
+
+
+if __name__ == '__main__':
+    AlunoData()
+    print('Suecesso na Conexão!')
